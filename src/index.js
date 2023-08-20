1305ac8713288c16dbd4d63288abcfcb77ccdf8d
theFutureGuy <you@example.com>
2023-08-20 19:40:31 +0530

    Main updated

--- src/index.js
+++ src/index.js
@@ -1,35 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from './pages/Home';
 import Crypto from './pages/Crypto';
 import Trending from './pages/Trending';
 import Saved from './pages/Saved';
+import CryptoDetails from './components/CryptoDetails';
 
-
-const router = createBrowserRouter(
-    [
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children:[
       {
-        path:"/" ,
-        element:<Home />,
-        children:[
+        path:"/",
+        element: <Crypto />,
+        children: [
           {
-            path:"/",
-            element:<Crypto /> 
-          },
-          {
-            path:"/trending",
-            element:<Trending /> 
-          },
-          {
-            path:"/saved",
-            element:<Saved /> 
+            path:":coinId",
+            element: <CryptoDetails />
           }
         ]
       },
-    ]);
+      {
+        path:"/trending",
+        element: <Trending />,
+        children: [
+          {
+            path:":coinId",
+            element: <CryptoDetails />
+          }
+        ]
+      },
+      {
+        path:"/saved",
+        element: <Saved />,
+        children: [
+          {
+            path:":coinId",
+            element: <CryptoDetails />
+          }
+        ]
+      }
+    ]
+
+
+  },
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
